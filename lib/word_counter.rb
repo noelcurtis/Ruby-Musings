86c99d082009ebf2cc7b48288872512f79e4149d
Noel Curtis <noelcurtis@gmail.com>
2012-04-12 17:20:24 -0400

    using a module..

--- lib/word_counter.rb
+++ lib/word_counter.rb
@@ -1,35 +1,39 @@
-class WordWrapper
-  include Comparable
-  attr_accessor :word, :count
-  
-  def <=>(an_other)
-     an_other.count <=> self.count
+module RubyMusings
+
+  class WordWrapper
+    include Comparable
+    attr_accessor :word, :count
+
+    def <=>(an_other)
+       an_other.count <=> self.count
+    end
   end
-end
 
-class WordCounter
-  
-  def self.count_words(file_name)
-    word_hash = Hash.new
-    
-    file = File.open(file_name, "r")
-    while (line = file.gets)
+  class WordCounter
 
-      line.scan(/\w+/).each do |word|
-        word.downcase!
-        if word_hash.has_key? word
-          word_hash[word].count += 1
-        else
-          new_word = WordWrapper.new
-          new_word.word = word
-          new_word.count = 1
-          word_hash[word] = new_word
+    def self.count_words(file_name)
+      word_hash = Hash.new
+
+      file = File.open(file_name, "r")
+      while (line = file.gets)
+
+        line.scan(/\w+/).each do |word|
+          word.downcase!
+          if word_hash.has_key? word
+            word_hash[word].count += 1
+          else
+            new_word = WordWrapper.new
+            new_word.word = word
+            new_word.count = 1
+            word_hash[word] = new_word
+          end
         end
       end
+
+      all_words = word_hash.values
+      all_words.sort!
     end
-    
-    all_words = word_hash.values
-    all_words.sort!
+
   end
-  
+
 end
--- spec/word_counter_spec.rb
+++ spec/word_counter_spec.rb
@@ -4,7 +4,7 @@ require 'word_counter.rb'
 describe "Count Words" do
 
   it "Should be able to count words in a file" do
-    words = WordCounter.count_words("../test_files/text_file.txt")
+    words = RubyMusings::WordCounter.count_words("../test_files/text_file.txt")
     words.count.should eql(8)
   end
 
